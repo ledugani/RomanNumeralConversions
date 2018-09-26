476afd2748640faa3a3044d341c5246a5c70b77f
Tom Dugan <tdugan85@gmail.com>
2018-09-25 20:58:43 -0500

    finished planning (i think)

--- Program.cs
+++ Program.cs
@@ -10,6 +10,48 @@ namespace RomanNumeralConversions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the number you'd like converted to Roman Numerals: ");
+            var userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out int result))
+            {
+                Console.WriteLine(result);
+                var x = Math.Floor(Math.Log10(result) + 1);
+                
+                switch (x)
+                {
+                    case 1:
+                        // convert to roman numeral ones
+                        break;
+
+                    case 2:
+                        // convert second digit to roman numeral ones
+                        // AND convert first digit to roman numeral tens
+                        break;
+
+                    case 3:
+                        // convert third digit to roman numeral ones
+                        // AND convert second digit to roman numeral tens
+                        // AND convert third digit to roman numeral hundreds
+                        break;
+
+                    case 4:
+                        // convert third digit to roman numeral ones
+                        // AND convert second digit to roman numeral tens
+                        // AND convert third digit to roman numeral hundreds
+                        // AND convert fourth digit to roman numeral thousands 
+                        break;
+
+                    default:
+                        Console.WriteLine("Whoa, friend! That number is TOO big!");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input not valid.");
+            }
+            Console.ReadLine();
         }
     }
 }
